87d58434aec3ace10e581ac8d42f309028c0c82a
MOUAYEDSB <sabbaghmouayed@gmail.com>
2023-10-02 17:22:36 +0100

    scripts

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+	const amenities = {};
+  
+	$('input[type="checkbox"]').change(function () {
+	  if ($(this).prop('checked')) {
+		amenities[$(this).attr('data-id')] = $(this).attr('data-name');
+	  } else {
+		delete amenities[$(this).attr('data-id')];
+	  }
+	  let text = '';
+	  for (const amenity in amenities) {
+		text += amenities[amenity] + ', ';
+	  }
+	  text = text.slice(0, -2); // remove last comma and space
+	  $('div.amenities h4').text(text);
+	});
+  });
+  
